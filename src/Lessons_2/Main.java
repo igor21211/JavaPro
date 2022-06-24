cc0303131308f177f411fd159b35d8571c38ed7a
Igor <spuraigor@gmail.com>
2022-06-24 20:57:52 +0300

    FirstCommit

---
+++ src/Lessons_2/Main.java
@@ -0,0 +1,13 @@
+package Lessons_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> str = new ArrayList<>();
+        str.add("abc");
+        str.add("abc");
+
+    }
+}
