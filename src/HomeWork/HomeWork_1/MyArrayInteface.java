b71bc337c888e0168332cf7098f200f1452d7e77
Igor <spuraigor@gmail.com>
2022-06-30 21:38:10 +0300

    HomeWork

---
+++ src/HomeWork/HomeWork_1/MyArrayInteface.java
@@ -0,0 +1,24 @@
+package HomeWork.HomeWork_1;
+
+import java.util.List;
+
+public interface MyArrayInteface {
+    void add(Integer I);
+
+    boolean add(int i, Integer I);
+
+    boolean removeByValue(Integer I);
+
+    boolean set(int i, Integer I);
+
+    void clear();
+
+    Integer get(int i);
+
+    boolean isEmpty();
+
+    boolean remove(int i);
+
+    int size();
+
+}
--- src/Lessons_2/ArrayLogic.java
+++ src/Lessons_2/ArrayLogic.java
@@ -1,13 +1,13 @@
 package Lessons_2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class ArrayLogic {
     public static void main(String[] args) {
 
 
-
+        List<String> str = new ArrayList<>();
+        str.get(1);
     }
 }
